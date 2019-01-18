c578ea6a405f16db91f343ade0351b39f9c74fe4
NileshChandekar <deejniels@gmail.com>
2019-01-18 20:51:10 +0530

    README

---
+++ emulatedstack_osp13_fake_pxe_bond_lacp/templates/deploy.sh
@@ -0,0 +1,1 @@
+openstack overcloud deploy --templates -e /home/stack/templates/node-info.yaml -e /home/stack/templates/overcloud_images.yaml -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml -e /home/stack/templates/network-environment.yaml --ntp-server 192.168.24.1
